8652c90c47a34c81f0b68c11a2d9c8bf27f56d22
mariyahomenko <73564203+mariyahomenko@users.noreply.github.com>
2021-03-08 11:42:53 +0300

    Add files via upload

---
+++ lgccrd.py
@@ -0,0 +1,517 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from das import Ui_Dialog
+
+app = QtWidgets.QApplication(sys.argv)
+Dialog = QtWidgets.QDialog()
+ui = Ui_Dialog()
+ui.setupUi(Dialog)
+Dialog.show()
+
+#2
+def chng_():
+    smthng = ui.pushButton.text()
+    if smthng == ' ':
+        ui.pushButton.setText('2')
+    else:
+        ui.pushButton.setText(' ')
+def chng_18():
+    smthng = ui.pushButton_18.text()
+    if smthng == ' ':
+        ui.pushButton_18.setText('2')
+    else:
+        ui.pushButton_18.setText(' ')
+def chng_31():
+    smthng = ui.pushButton_31.text()
+    if smthng == ' ':
+        ui.pushButton_31.setText('2')
+    else:
+        ui.pushButton_31.setText(' ')
+def chng_44():
+    smthng = ui.pushButton_44.text()
+    if smthng == ' ':
+        ui.pushButton_44.setText('2')
+    else:
+        ui.pushButton_44.setText(' ')
+
+#3
+def chng_2():
+    smthng = ui.pushButton_2.text()
+    if smthng == ' ':
+        ui.pushButton_2.setText('3')
+    else:
+        ui.pushButton_2.setText(' ')
+def chng_19():
+    smthng = ui.pushButton_19.text()
+    if smthng == ' ':
+        ui.pushButton_19.setText('3')
+    else:
+        ui.pushButton_19.setText(' ')
+def chng_32():
+    smthng = ui.pushButton_32.text()
+    if smthng == ' ':
+        ui.pushButton_32.setText('3')
+    else:
+        ui.pushButton_32.setText(' ')
+def chng_45():
+    smthng = ui.pushButton_45.text()
+    if smthng == ' ':
+        ui.pushButton_45.setText('3')
+    else:
+        ui.pushButton_45.setText(' ')
+
+#4
+def chng_3():
+    smthng = ui.pushButton_3.text()
+    if smthng == ' ':
+        ui.pushButton_3.setText('4')
+    else:
+        ui.pushButton_3.setText(' ')
+def chng_20():
+    smthng = ui.pushButton_20.text()
+    if smthng == ' ':
+        ui.pushButton_20.setText('4')
+    else:
+        ui.pushButton_20.setText(' ')
+def chng_33():
+    smthng = ui.pushButton_33.text()
+    if smthng == ' ':
+        ui.pushButton_33.setText('4')
+    else:
+        ui.pushButton_33.setText(' ')
+def chng_46():
+    smthng = ui.pushButton_46.text()
+    if smthng == ' ':
+        ui.pushButton_46.setText('4')
+    else:
+        ui.pushButton_46.setText(' ')
+
+#5
+def chng_4():
+    smthng = ui.pushButton_4.text()
+    if smthng == ' ':
+        ui.pushButton_4.setText('5')
+    else:
+        ui.pushButton_4.setText(' ')
+def chng_21():
+    smthng = ui.pushButton_21.text()
+    if smthng == ' ':
+        ui.pushButton_21.setText('5')
+    else:
+        ui.pushButton_21.setText(' ')
+def chng_34():
+    smthng = ui.pushButton_34.text()
+    if smthng == ' ':
+        ui.pushButton_34.setText('5')
+    else:
+        ui.pushButton_34.setText(' ')
+def chng_47():
+    smthng = ui.pushButton_47.text()
+    if smthng == ' ':
+        ui.pushButton_47.setText('5')
+    else:
+        ui.pushButton_47.setText(' ')
+
+#6
+def chng_5():
+    smthng = ui.pushButton_5.text()
+    if smthng == ' ':
+        ui.pushButton_5.setText('6')
+    else:
+        ui.pushButton_5.setText(' ')
+def chng_22():
+    smthng = ui.pushButton_22.text()
+    if smthng == ' ':
+        ui.pushButton_22.setText('6')
+    else:
+        ui.pushButton_22.setText(' ')
+def chng_35():
+    smthng = ui.pushButton_35.text()
+    if smthng == ' ':
+        ui.pushButton_35.setText('6')
+    else:
+        ui.pushButton_35.setText(' ')
+def chng_48():
+    smthng = ui.pushButton_48.text()
+    if smthng == ' ':
+        ui.pushButton_48.setText('6')
+    else:
+        ui.pushButton_48.setText(' ')
+
+#7
+def chng_6():
+    smthng = ui.pushButton_6.text()
+    if smthng == ' ':
+        ui.pushButton_6.setText('7')
+    else:
+        ui.pushButton_6.setText(' ')
+def chng_23():
+    smthng = ui.pushButton_23.text()
+    if smthng == ' ':
+        ui.pushButton_23.setText('7')
+    else:
+        ui.pushButton_23.setText(' ')
+def chng_36():
+    smthng = ui.pushButton_36.text()
+    if smthng == ' ':
+        ui.pushButton_36.setText('7')
+    else:
+        ui.pushButton_36.setText(' ')
+def chng_49():
+    smthng = ui.pushButton_49.text()
+    if smthng == ' ':
+        ui.pushButton_49.setText('7')
+    else:
+        ui.pushButton_49.setText(' ')
+
+#8
+def chng_7():
+    smthng = ui.pushButton_7.text()
+    if smthng == ' ':
+        ui.pushButton_7.setText('8')
+    else:
+        ui.pushButton_7.setText(' ')
+def chng_24():
+    smthng = ui.pushButton_24.text()
+    if smthng == ' ':
+        ui.pushButton_24.setText('8')
+    else:
+        ui.pushButton_24.setText(' ')
+def chng_37():
+    smthng = ui.pushButton_37.text()
+    if smthng == ' ':
+        ui.pushButton_37.setText('8')
+    else:
+        ui.pushButton_37.setText(' ')
+def chng_50():
+    smthng = ui.pushButton_50.text()
+    if smthng == ' ':
+        ui.pushButton_50.setText('8')
+    else:
+        ui.pushButton_50.setText(' ')
+
+#9
+def chng_8():
+    smthng = ui.pushButton_8.text()
+    if smthng == ' ':
+        ui.pushButton_8.setText('9')
+    else:
+        ui.pushButton_8.setText(' ')
+def chng_25():
+    smthng = ui.pushButton_25.text()
+    if smthng == ' ':
+        ui.pushButton_25.setText('9')
+    else:
+        ui.pushButton_25.setText(' ')
+def chng_38():
+    smthng = ui.pushButton_38.text()
+    if smthng == ' ':
+        ui.pushButton_38.setText('9')
+    else:
+        ui.pushButton_38.setText(' ')
+def chng_51():
+    smthng = ui.pushButton_51.text()
+    if smthng == ' ':
+        ui.pushButton_51.setText('9')
+    else:
+        ui.pushButton_51.setText(' ')
+
+#10
+def chng_9():
+    smthng = ui.pushButton_9.text()
+    if smthng == ' ':
+        ui.pushButton_9.setText('10')
+    else:
+        ui.pushButton_9.setText(' ')
+def chng_26():
+    smthng = ui.pushButton_26.text()
+    if smthng == ' ':
+        ui.pushButton_26.setText('10')
+    else:
+        ui.pushButton_26.setText(' ')
+def chng_39():
+    smthng = ui.pushButton_39.text()
+    if smthng == ' ':
+        ui.pushButton_39.setText('10')
+    else:
+        ui.pushButton_39.setText(' ')
+def chng_52():
+    smthng = ui.pushButton_52.text()
+    if smthng == ' ':
+        ui.pushButton_52.setText('10')
+    else:
+        ui.pushButton_52.setText(' ')
+
+#v
+def chng_10():
+    smthng = ui.pushButton_10.text()
+    if smthng == ' ':
+        ui.pushButton_10.setText('В')
+    else:
+        ui.pushButton_10.setText(' ')
+def chng_27():
+    smthng = ui.pushButton_27.text()
+    if smthng == ' ':
+        ui.pushButton_27.setText('В')
+    else:
+        ui.pushButton_27.setText(' ')
+def chng_40():
+    smthng = ui.pushButton_40.text()
+    if smthng == ' ':
+        ui.pushButton_40.setText('В')
+    else:
+        ui.pushButton_40.setText(' ')
+def chng_53():
+    smthng = ui.pushButton_53.text()
+    if smthng == ' ':
+        ui.pushButton_53.setText('В')
+    else:
+        ui.pushButton_53.setText(' ')
+
+#d
+def chng_11():
+    smthng = ui.pushButton_11.text()
+    if smthng == ' ':
+        ui.pushButton_11.setText('Д')
+    else:
+        ui.pushButton_11.setText(' ')
+def chng_28():
+    smthng = ui.pushButton_28.text()
+    if smthng == ' ':
+        ui.pushButton_28.setText('Д')
+    else:
+        ui.pushButton_28.setText(' ')
+def chng_41():
+    smthng = ui.pushButton_41.text()
+    if smthng == ' ':
+        ui.pushButton_41.setText('Д')
+    else:
+        ui.pushButton_41.setText(' ')
+def chng_54():
+    smthng = ui.pushButton_54.text()
+    if smthng == ' ':
+        ui.pushButton_54.setText('Д')
+    else:
+        ui.pushButton_54.setText(' ')
+
+#k
+def chng_12():
+    smthng = ui.pushButton_12.text()
+    if smthng == ' ':
+        ui.pushButton_12.setText('К')
+    else:
+        ui.pushButton_12.setText(' ')
+def chng_29():
+    smthng = ui.pushButton_29.text()
+    if smthng == ' ':
+        ui.pushButton_29.setText('К')
+    else:
+        ui.pushButton_29.setText(' ')
+def chng_42():
+    smthng = ui.pushButton_42.text()
+    if smthng == ' ':
+        ui.pushButton_42.setText('К')
+    else:
+        ui.pushButton_42.setText(' ')
+def chng_55():
+    smthng = ui.pushButton_55.text()
+    if smthng == ' ':
+        ui.pushButton_55.setText('К')
+    else:
+        ui.pushButton_55.setText(' ')
+
+#t
+def chng_13():
+    smthng = ui.pushButton_13.text()
+    if smthng == ' ':
+        ui.pushButton_13.setText('Т')
+    else:
+        ui.pushButton_13.setText(' ')
+def chng_30():
+    smthng = ui.pushButton_30.text()
+    if smthng == ' ':
+        ui.pushButton_30.setText('Т')
+    else:
+        ui.pushButton_30.setText(' ')
+def chng_43():
+    smthng = ui.pushButton_43.text()
+    if smthng == ' ':
+        ui.pushButton_43.setText('Т')
+    else:
+        ui.pushButton_43.setText(' ')
+def chng_56():
+    smthng = ui.pushButton_56.text()
+    if smthng == ' ':
+        ui.pushButton_56.setText('Т')
+    else:
+        ui.pushButton_56.setText(' ')
+
+#24
+def chng_14():
+    smthng = ui.pushButton_14.text()
+    if smthng == ' ':
+        ui.retranslateUi(Dialog)
+    elif ui.pushButton_15.text() == ' ':
+        ui.retranslateUi(Dialog)
+    else:
+        ui.pushButton_16.setText('52')
+        ui.pushButton_14.setText(' ')
+        ui.pushButton.setText(' ')
+        ui.pushButton_2.setText(' ')
+        ui.pushButton_3.setText(' ')
+        ui.pushButton_4.setText(' ')
+        ui.pushButton_5.setText(' ')
+        ui.pushButton_6.setText(' ')
+        ui.pushButton_7.setText(' ')
+        ui.pushButton_18.setText(' ')
+        ui.pushButton_19.setText(' ')
+        ui.pushButton_20.setText(' ')
+        ui.pushButton_21.setText(' ')
+        ui.pushButton_22.setText(' ')
+        ui.pushButton_23.setText(' ')
+        ui.pushButton_24.setText(' ')
+        ui.pushButton_31.setText(' ')
+        ui.pushButton_32.setText(' ')
+        ui.pushButton_33.setText(' ')
+        ui.pushButton_34.setText(' ')
+        ui.pushButton_35.setText(' ')
+        ui.pushButton_36.setText(' ')
+        ui.pushButton_37.setText(' ')
+        ui.pushButton_44.setText(' ')
+        ui.pushButton_45.setText(' ')
+        ui.pushButton_46.setText(' ')
+        ui.pushButton_47.setText(' ')
+        ui.pushButton_48.setText(' ')
+        ui.pushButton_49.setText(' ')
+        ui.pushButton_50.setText(' ')
+
+#36
+def chng_15():
+    smthng = ui.pushButton_15.text()
+    if smthng == ' ':
+        ui.retranslateUi(Dialog)
+    elif ui.pushButton_14.text() == ' ':
+        ui.retranslateUi(Dialog)
+    else:
+        ui.pushButton_16.setText('52')
+        ui.pushButton_15.setText(' ')
+        ui.pushButton.setText(' ')
+        ui.pushButton_2.setText(' ')
+        ui.pushButton_3.setText(' ')
+        ui.pushButton_4.setText(' ')
+        ui.pushButton_18.setText(' ')
+        ui.pushButton_19.setText(' ')
+        ui.pushButton_20.setText(' ')
+        ui.pushButton_21.setText(' ')
+        ui.pushButton_31.setText(' ')
+        ui.pushButton_32.setText(' ')
+        ui.pushButton_33.setText(' ')
+        ui.pushButton_34.setText(' ')
+        ui.pushButton_44.setText(' ')
+        ui.pushButton_45.setText(' ')
+        ui.pushButton_46.setText(' ')
+        ui.pushButton_47.setText(' ')
+
+#52
+def chng_16():
+    smthng = ui.pushButton_16.text()
+    if smthng == ' ':
+        ui.retranslateUi(Dialog)
+    elif ui.pushButton_15.text() == ' ':
+        ui.retranslateUi(Dialog)
+    elif ui.pushButton_14.text() == ' ':
+        ui.retranslateUi(Dialog)
+    else:
+        ui.pushButton_16.setText(' ')
+
+#2
+ui.pushButton.clicked.connect(lambda: chng_())
+ui.pushButton_18.clicked.connect(lambda: chng_18())
+ui.pushButton_31.clicked.connect(lambda: chng_31())
+ui.pushButton_44.clicked.connect(lambda: chng_44())
+
+#3
+ui.pushButton_2.clicked.connect(lambda: chng_2())
+ui.pushButton_19.clicked.connect(lambda: chng_19())
+ui.pushButton_32.clicked.connect(lambda: chng_32())
+ui.pushButton_45.clicked.connect(lambda: chng_45())
+
+#4
+ui.pushButton_3.clicked.connect(lambda: chng_3())
+ui.pushButton_20.clicked.connect(lambda: chng_20())
+ui.pushButton_33.clicked.connect(lambda: chng_33())
+ui.pushButton_46.clicked.connect(lambda: chng_46())
+
+#5
+ui.pushButton_4.clicked.connect(lambda: chng_4())
+ui.pushButton_21.clicked.connect(lambda: chng_21())
+ui.pushButton_34.clicked.connect(lambda: chng_34())
+ui.pushButton_47.clicked.connect(lambda: chng_47())
+
+#6
+ui.pushButton_5.clicked.connect(lambda: chng_5())
+ui.pushButton_22.clicked.connect(lambda: chng_22())
+ui.pushButton_35.clicked.connect(lambda: chng_35())
+ui.pushButton_48.clicked.connect(lambda: chng_48())
+
+#7
+ui.pushButton_6.clicked.connect(lambda: chng_6())
+ui.pushButton_23.clicked.connect(lambda: chng_23())
+ui.pushButton_36.clicked.connect(lambda: chng_36())
+ui.pushButton_49.clicked.connect(lambda: chng_49())
+
+#8
+ui.pushButton_7.clicked.connect(lambda: chng_7())
+ui.pushButton_24.clicked.connect(lambda: chng_24())
+ui.pushButton_37.clicked.connect(lambda: chng_37())
+ui.pushButton_50.clicked.connect(lambda: chng_50())
+
+#9
+ui.pushButton_8.clicked.connect(lambda: chng_8())
+ui.pushButton_25.clicked.connect(lambda: chng_25())
+ui.pushButton_38.clicked.connect(lambda: chng_38())
+ui.pushButton_51.clicked.connect(lambda: chng_51())
+
+#10
+ui.pushButton_9.clicked.connect(lambda: chng_9())
+ui.pushButton_26.clicked.connect(lambda: chng_26())
+ui.pushButton_39.clicked.connect(lambda: chng_39())
+ui.pushButton_52.clicked.connect(lambda: chng_52())
+
+#v
+ui.pushButton_10.clicked.connect(lambda: chng_10())
+ui.pushButton_27.clicked.connect(lambda: chng_27())
+ui.pushButton_40.clicked.connect(lambda: chng_40())
+ui.pushButton_53.clicked.connect(lambda: chng_53())
+
+#d
+ui.pushButton_11.clicked.connect(lambda: chng_11())
+ui.pushButton_28.clicked.connect(lambda: chng_28())
+ui.pushButton_41.clicked.connect(lambda: chng_41())
+ui.pushButton_54.clicked.connect(lambda: chng_54())
+
+#k
+ui.pushButton_12.clicked.connect(lambda: chng_12())
+ui.pushButton_29.clicked.connect(lambda: chng_29())
+ui.pushButton_42.clicked.connect(lambda: chng_42())
+ui.pushButton_55.clicked.connect(lambda: chng_55())
+
+#t
+ui.pushButton_13.clicked.connect(lambda: chng_13())
+ui.pushButton_30.clicked.connect(lambda: chng_30())
+ui.pushButton_43.clicked.connect(lambda: chng_43())
+ui.pushButton_56.clicked.connect(lambda: chng_56())
+
+#24
+ui.pushButton_14.clicked.connect(lambda: chng_14())
+
+#36
+ui.pushButton_15.clicked.connect(lambda: chng_15())
+
+#52
+ui.pushButton_16.clicked.connect(lambda: chng_16())
+
+#newgame
+ui.pushButton_57.clicked.connect(lambda: ui.retranslateUi(Dialog))
+
+sys.exit(app.exec_())
